5ffeb8f3e39dc25e7ab492b9e538225d64677035
YoussefZinelabidine <youssef.zinelabidine@hotmail.fr>
2024-04-04 10:41:35 +0100

    testing pygod gcnae model

--- sandBox3.py
+++ sandBox3.py
@@ -1,36 +1,26 @@
-def train_eval_ae(tab_path, batch_size=8192, attribute_dims=None):
+def train_eval_gcnae(graph_path, batch_size=0, attribute_dims=None):
     import warnings
     warnings.simplefilter(action='ignore')
 
     import pandas as pd
     import numpy as np
-
-    from functions.training_utils import recall_at_k, Dataset
-    from models.AE import AE
-
-    import gc
     import torch
-    from sklearn.preprocessing import StandardScaler
-    from pygod.metric import eval_roc_auc
+    from pygod.models import GCNAE
+    from pygod.utils import data_loader
+    from pygod.metric import eval_roc_auc, recall_at_k
     from pythresh.thresholds.iqr import IQR
-    from sklearn.metrics import precision_recall_curve
-    from sklearn.metrics import auc
-    from sklearn.metrics import f1_score
-
-    from torch_geometric import data
-
-    import os
-    from os import listdir
-    from os.path import isfile, join
-    import pickle
+    from sklearn.metrics import precision_recall_curve, auc, f1_score
+    from torch_geometric.transforms import GCNNorm
     from tqdm import tqdm
-
     import time
+    import os
 
     start = time.time()
 
     if not os.path.exists('results'):
         os.makedirs('results')
+
+    transform = GCNNorm()
 
     # Initialize lists to store the evaluation metrics
     auc_roc_list = []
@@ -38,104 +28,66 @@ def train_eval_ae(tab_path, batch_size=8192, attribute_dims=None):
     r_10_list = []
     f1_list = []
     hit_rate_list = []
-    recall_at_10_list_per_type = []  # Initialize a list to store the Recall@10 per type
+    recall_at_10_list_per_type = []
 
-    files = [f for f in listdir(tab_path) if isfile(join(tab_path, f))]
-    
-    chunksize = 1000
+    files = [f for f in os.listdir(graph_path) if os.path.isfile(os.path.join(graph_path, f))]
 
     for filename in tqdm(files):
-        file_path = tab_path + filename
-        i= 0
-        
-        for chunk in pd.read_csv(file_path, dtype={'exec_id': int, 'y': int}, chunksize=chunksize):
-            print("chunk n: ",i)
-            i +=1
-            df_tab = chunk
-            df_tab = df_tab.sort_values(by=['exec_id', 'elapsed_time'])
-            original_y = df_tab[['event_id', 'exec_id', 'y']]
-            df_tab.y = (df_tab.y > 0).astype(int)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-            X = df_tab.drop(columns=['event_id', 'elapsed_time', 'y'])
-            X = X.pivot_table(index='exec_id', columns=X.groupby('exec_id').cumcount())
-            X = X.sort_index(axis='columns', level=1)
-            X.columns = X.columns.map('{0[0]}|{0[1]}'.format)
-            X_mask = X.isnull()
-            X = X.fillna(0)
+        file_path = os.path.join(graph_path, filename)
 
-            # Convert DataFrame to tensor
-            scaler = StandardScaler()
-            X_scaled = scaler.fit_transform(X)
-            X = pd.DataFrame(data=X_scaled, columns=X.columns)
+        data_loader_instance = data_loader(file_path, device, transform)
 
-            # Prepare Dataset Object
-            dataset = Dataset(X, X.iloc[:, :1], df_tab.groupby('exec_id').size().max(), attribute_dims, X_mask)
+        model = GCNAE(device=device, encoder_layers=2, decoder_layers=1, attribute_dims=attribute_dims)
+        model.fit(data_loader_instance, batch_size=batch_size)
 
-            # Instantiate AE class
-            ae = AE()
+        decision_scores = model.decision_score_
+        thres = IQR()
+        labels = thres.eval(decision_scores)
 
-            # Construct the model with your dataset
-            model, features, _ = ae.model_fn(dataset, **ae.config)
+        # Metrics
+        auc_roc = eval_roc_auc(data_loader_instance.get_labels().cpu().numpy(), decision_scores)
+        precision, recall, _ = precision_recall_curve(data_loader_instance.get_labels().cpu().numpy(), decision_scores)
+        auc_pr = auc(recall, precision)
+        recall_at_10 = recall_at_k(data_loader_instance.get_labels().cpu().numpy(), decision_scores, 10)
+        f1 = f1_score(data_loader_instance.get_labels().cpu().numpy(), labels)
 
-            # Train the model
-            model.fit(features, features, epochs=100, batch_size=batch_size, verbose=False)
+        # Append the evaluation metrics to the corresponding lists
+        auc_roc_list.append(auc_roc)
+        auc_pr_list.append(auc_pr)
+        r_10_list.append(recall_at_10)
+        f1_list.append(f1)
 
-            # Assign the trained model to the DAE instance
-            ae.model = model
+        # Add predicted binary labels to the DataFrame
+        nodes_y_df = data_loader_instance.get_node_y_df()
+        nodes_y_df["predicted_binary"] = labels
+        nodes_y_df["correct"] = ((nodes_y_df["predicted_binary"] == 1) & (nodes_y_df["original_y"] > 0)) | (
+                    (nodes_y_df["predicted_binary"] == 0) & (nodes_y_df["original_y"] == 0))
 
-            # Now you can use your trained model to detect anomalies
-            decision_scores = ae.detect(dataset)
-
-            # Apply threshold
-            thres = IQR()
-            labels = thres.eval(decision_scores)
-
-            # Metrics
-            auc_roc = eval_roc_auc(df_tab.y, decision_scores)
-            precision, recall, _ = precision_recall_curve(df_tab.y, decision_scores)
-            auc_pr = auc(recall, precision)
-            recall_at_10 = recall_at_k(df_tab.y, decision_scores, 10)
-            f1 = f1_score(df_tab.y, labels)
-
-            # Append the evaluation metrics to the corresponding lists
-            auc_roc_list.append(auc_roc)
-            auc_pr_list.append(auc_pr)
-            r_10_list.append(recall_at_10)
-            f1_list.append(f1)
-
-            # Add predicted binary labels to the DataFrame
-            original_y["predicted_binary"] = labels
-            original_y["correct"] = ((original_y["predicted_binary"] == 1) & (original_y["y"] > 0)) | (
-                        (original_y["predicted_binary"] == 0) & (original_y["y"] == 0))
-
-            # Calculate hit rates for each original value and store them in a dictionary
-            hit_rates = original_y.groupby("y")["correct"].mean().to_dict()
-            hit_rate_list.append(hit_rates)
+        # Calculate hit rates for each original value and store them in a dictionary
+        hit_rates = nodes_y_df.groupby("original_y")["correct"].mean().to_dict()
+        hit_rate_list.append(hit_rates)
 
         # Compute Recall @ 10 for each type
-            # Step 1: Determine the threshold for the top 10%
-            threshold = np.percentile(decision_scores, 90)
+        # Step 1: Determine the threshold for the top 10%
+        threshold = np.percentile(decision_scores.cpu().numpy(), 90)
 
-            # Step 2: Use the threshold to set positive/negative predictions
-            predicted_binary = (decision_scores >= threshold).astype(int)
+        # Step 2: Use the threshold to set positive/negative predictions
+        predicted_binary = (decision_scores >= threshold).astype(int)
 
-            # Step 3: Compute Recall for each class
-            recalls_for_each_class = {}
+        # Step 3: Compute Recall for each class
+        recalls_for_each_class = {}
 
-            original_y_values = original_y['y'].values
-            predicted_binary = np.array(predicted_binary)
+        original_y = data_loader_instance.get_labels().cpu().numpy()
+        unique_classes = np.unique(original_y)  # Get the unique classes
+        for cls in unique_classes:
+            true_positives = np.sum((original_y == cls) & (predicted_binary == 1))
+            actual_positives = np.sum(original_y == cls)
+            recall = true_positives / (actual_positives + 1e-10)  # Add a small value to prevent division by zero
+            recalls_for_each_class[cls] = recall
 
-            unique_classes = np.unique(original_y_values)  # Get the unique classes
-            for cls in unique_classes:
-                true_positives = np.sum((original_y_values == cls) & (predicted_binary == 1))
-                actual_positives = np.sum(original_y_values == cls)
-                recall = true_positives / (actual_positives + 1e-10)  # Add a small value to prevent division by zero
-                recalls_for_each_class[cls] = recall
-
-
-            recall_at_10_list_per_type.append(recalls_for_each_class)
-
-            gc.collect()
+        recall_at_10_list_per_type.append(recalls_for_each_class)
 
     results_dict = {'F1': f1_list,
                     'AUC ROC': auc_roc_list,
@@ -166,8 +118,8 @@ def train_eval_ae(tab_path, batch_size=8192, attribute_dims=None):
         print('{:<25s} {:<10.1f} {:<10.1f}'.format(col, results_mean[col] * 100, results_std[col] * 100))
 
     # Save the results to a CSV file
-    results_df.to_csv('results/' + tab_path.strip('/').split('/')[-1] + '_ae_results.csv', index=False)
-    hit_rate_df.to_csv('results/' + tab_path.strip('/').split('/')[-1] + '_ae_hitrate.csv', index=False)
+    results_df.to_csv('results/' + os.path.basename(graph_path).strip('/').split('/')[-1] + '_gcnae_results.csv', index=False)
+    hit_rate_df.to_csv('results/' + os.path.basename(graph_path).strip('/').split('/')[-1] + '_gcnae_hitrate.csv', index=False)
 
     # Print hit rate mean and standard deviation
     print('')
